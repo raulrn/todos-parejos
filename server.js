985f2cdf844744042ef382c34884cf968fd5ee93
Raul <gypsydanger@jaeger.kaiju>
2016-05-07 13:44:51 -0500

    Mongo corriendo

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var app = express();
+//Lets load the mongoose module in our program
+var mongoose = require('mongoose');
+
+
 
 // var router = require('./router');
 
@@ -9,6 +13,13 @@ function initializate ()
 	app.listen(1818, function() 
 	{
 		console.log("Node server running on http://localhost:1818");
+	});
+	
+	//Lets connect to our database using the DB server URL.
+	mongoose.connect('mongodb://localhost/allequal', function(err, res) 
+	{
+		if(err) throw err;
+		console.log('Connected to Database');
 	});
 }
 
